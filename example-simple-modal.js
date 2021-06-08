3f04fa797951b0a69d61b8f7f0af34a88ba398a3
jeremycos <73873125+jeremycos@users.noreply.github.com>
2021-06-08 11:16:53 +0300

    Update example-simple-modal.js

--- example-simple-modal.js
+++ example-simple-modal.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { useModal, Modal as ModalSimple } from "@/components/ui/Modal";
+import { useModal, Modal} from "@/components/ui/Modal";
 
 export default function ExamplePage() {
   const { openModal, closeModal } = useModal();
   return (
     <div>
       <a onClick={() => openModal()}>Open Simple Modal</a>
-      <ModalSimple closeModal={closeModal} title="Modal title here">Modal text here</Modal>
+      <Modal closeModal={closeModal} title="Modal title here">Modal text here</Modal>
     </div>
   );
 }
